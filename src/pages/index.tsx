83bcf42951c0978524f8cb31930675188413c044
Antoine Abhay <antoineabhay@gmail.com>
2022-10-29 16:47:25 +0200

    Improve index page layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -41,40 +41,38 @@ const Home: NextPage = () => {
               layout="fill"
             />
           </div>
-          <div className="font-header pl-[5vw] pt-[calc(45vh-72px)] text-7xl font-medium leading-tight text-gray-50">
-            <div>Hey,</div>
+          <div className="h-screen flex flex-col justify-center font-header pl-[5vw] text-7xl font-medium leading-tight text-gray-50">
+            <div className="mt-8">Hey,</div>
             <div>I&apos;m Antoine</div>
           </div>
         </div>
-        <div className="min-h-screen w-screen bg-gray-50">
-          <div className="flex flex-col items-center">
-            <div className="font-header mt-24 text-5xl font-medium leading-relaxed">
-              Who I am
-            </div>
-            <div className="h-1 w-20 bg-red-600" />
-            <div
-              ref={profileRef}
-              className="flex w-full flex-col items-center md:mt-24 md:flex-row md:justify-around max-w-6xl"
-            >
-              <ProfileItem
-                imageSrc="/images/dev.inline.svg"
-                alt="A developer"
-                title="Developer"
-                description="Building products people love"
-              />
-              <ProfileItem
-                imageSrc="/images/space.inline.svg"
-                alt="An astronaut"
-                title="Space enthousiast"
-                description="Dreaming of the stars"
-              />
-              <ProfileItem
-                imageSrc="/images/cyclist.inline.svg"
-                alt="A cyclist"
-                title="Urban cyclist"
-                description="Riding in Paris"
-              />
-            </div>
+        <div className="flex min-h-screen w-screen flex-col items-center justify-center bg-gray-50">
+          <div className="font-header text-5xl font-medium leading-relaxed">
+            Who I am
+          </div>
+          <div className="h-1 w-20 bg-red-600" />
+          <div
+            ref={profileRef}
+            className="flex w-full max-w-6xl flex-col items-center md:mt-[5vw] md:flex-row"
+          >
+            <ProfileItem
+              imageSrc="/images/dev.inline.svg"
+              alt="A developer"
+              title="Developer"
+              description="Building products people love"
+            />
+            <ProfileItem
+              imageSrc="/images/space.inline.svg"
+              alt="An astronaut"
+              title="Space enthousiast"
+              description="Dreaming of the stars"
+            />
+            <ProfileItem
+              imageSrc="/images/cyclist.inline.svg"
+              alt="A cyclist"
+              title="Urban cyclist"
+              description="Riding in Paris"
+            />
           </div>
         </div>
       </main>
@@ -110,7 +108,7 @@ const ProfileItem = ({
           transform: translateX(0);
         }
       `}</style>
-      <div className="item mb-12 text-center">
+      <div className="item mb-12 text-center md:w-2/6">
         <Image src={imageSrc} alt={alt} height={140} width={113} />
         <div className="text-2xl">{title}</div>
         <div className="text-base">{description}</div>
