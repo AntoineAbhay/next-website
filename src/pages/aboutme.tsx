00dcaa9557abfb603795da2562f6add475b3c280
Antoine Abhay <antoineabhay@gmail.com>
2022-10-31 00:32:44 +0100

    Better github projects layout

--- src/pages/aboutme.tsx
+++ src/pages/aboutme.tsx
@@ -16,10 +16,10 @@ const AboutMe: NextPage<{ repositories: Repository[] }> = ({
   repositories,
 }) => {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center m-4">
       <section>
-        <h2 className="text-3xl font-bold">Github</h2>
-        <div className="grid max-w-3xl sm:grid-cols-2">
+        <h2 className="text-3xl font-bold">My projects on Github</h2>
+        <div className="grid max-w-3xl sm:grid-cols-2 lg:grid-cols-3 lg:max-w-5xl">
           {repositories.map((repository, index) => (
             <RepositoryCard key={index} repository={repository} />
           ))}
